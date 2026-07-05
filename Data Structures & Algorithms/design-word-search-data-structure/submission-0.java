c872cbdba3d4e544e78975df1c5a2cb41a3af9f9
shubhammidha <165390830+shubhammidha@users.noreply.github.com>
2026-07-05 09:52:40 +0530

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.java
@@ -0,0 +1,56 @@
+class TrieNode{
+    TrieNode children[];
+    boolean word;
+
+    public TrieNode(){
+        children = new TrieNode[26];
+        word = false;
+    }
+}
+
+class WordDictionary {
+    private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+        for(char c: word.toCharArray()){
+            if(curr.children[c - 'a'] == null){
+                curr.children[c - 'a'] = new TrieNode();
+            }
+            curr = curr.children[c - 'a'];
+        }
+        curr.word = true;
+    }
+
+    public boolean search(String word) {
+        return dfs(word, 0, root);
+    }
+
+    private boolean dfs(String word, int j, TrieNode root){
+        TrieNode curr = root;
+
+        for(int i=j; i<word.length(); i++){
+
+            char c = word.charAt(i);
+            if(c == '.'){
+                for(TrieNode child: curr.children){
+                    if(child != null && dfs(word, i+1, child)){
+                        return true;
+                    }
+                }
+                return false;
+            }
+            else{
+                if(curr.children[c - 'a'] == null){
+                    return false;
+                }
+                curr = curr.children[c - 'a'];
+            }
+        }
+        return curr.word;
+    }
+}
