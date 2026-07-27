285ada04e9d2473b71881ee85c8da7bae1ef7bd8
shubhammidha <165390830+shubhammidha@users.noreply.github.com>
2026-07-27 13:00:16 +0530

    Add: palindromic-substrings - submission-2

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-2.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        int count = 0;
+
+        boolean[][] dp = new boolean[n][n];
+
+        for(int i=0; i<n; i++){
+            dp[i][i] = true;
+            count += 1;
+        }
+
+        int maxLen = 1;
+        int start = 0;
+
+        for(int len = 2; len<=n; len++){
+            for(int i=0; i<n-len+1; i++){
+                
+                int j = i + len-1;
+
+                if(s.charAt(i) == s.charAt(j)){
+                    if(len == 2 || dp[i+1][j-1]){
+                        dp[i][j] = true;
+                        count+=1;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
